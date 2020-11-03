0e27a3f54335e707ee65a428301e0abfb458a6f1
andrew <chibarandrey@gmail.com>
2020-11-03 18:19:10 +0300

    dinamic variables added

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,19 @@
       <h1 class="text-4 font-semibold text-gray-800 mb-8">
         The main index page
       </h1>
+      <button
+        class="bg-gray-700 p-4 border text-white"
+        @click="characterId = characterId + 1"
+      >
+        Next character
+      </button>
+      <button
+        class="bg-yellow-700 p-4 border text-white"
+        @click="characterId = characterId - 1"
+      >
+        Previous character
+      </button>
+      <h3 class="text-2xl">{{ character.name }} id: {{ character.id }}</h3>
       <ul>
         <li v-for="character in characters.results" :key="character.id">
           {{ character.name }}
@@ -18,6 +31,11 @@
 import gql from 'graphql-tag'
 
 export default {
+  data() {
+    return {
+      characterId: 1,
+    }
+  },
   apollo: {
     characters: gql`
       query getCharacters {
@@ -30,6 +48,21 @@ export default {
         }
       }
     `,
+    character: {
+      query: gql`
+        query getCharacter($id: ID!) {
+          character(id: $id) {
+            id
+            name
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.characterId,
+        }
+      },
+    },
   },
 }
 </script>
